3f8ec765dd99fea36013bd734c1de35c81e9df38
Borlaff <asborlaff@gmail.com>
2026-04-27 15:21:24 -0700

    Adding cone_search

--- ephessos/core.py
+++ ephessos/core.py
@@ -1,10 +1,32 @@
-from astropy.time import Time
 from astropy.table import vstack
 import astropy.units as u
 import numpy as np
 import pandas as pd
+from astropy.time import Time
+import ephessos as ep
+import os
+ephessos_dir = os.path.dirname(ep.__file__)
 
-def ephessos(designation, epoch=None, eccentricity=None, node=None, arg_perihelion=None, inclination=None, mean_anomaly=None, semimajor_axis=None, mean_motion=None, mjd_start=58849.0, mjd_end=61042.0, step_size="1d", verbose=False):
+def ephessos(designation="Default", epoch=None, eccentricity=None, node=None, arg_perihelion=None, inclination=None, mean_anomaly=None, semimajor_axis=None, mean_motion=None, mjd_start=58849.0, mjd_end=61042.0, step_size="1d", quantities='1,2,3,4,5,6,7,8,9,10,19,20,23,24,25,27,29', verbose=False):
+    """
+    ephessos is a function that queries the JPL Horizons API for ephemeris data based on user-provided orbital elements and time range. The function constructs a request URL with the specified parameters, sends the request to the Horizons API, and processes the response to extract the relevant ephemeris data. The resulting data is returned as a pandas DataFrame containing the requested quantities for each time step within the specified range. The function also includes error handling and verbose output options for debugging and user feedback.
+        :param str designation: Name of the object to query (default is "Default")
+        :param str epoch: Julian Day number (JDTDB) of osculating elements (default is None)
+        :param float eccentricity: Eccentricity of the orbit (default is None)
+        :param float node: Longitude of ascending node wrt ecliptic in degrees (default is None)
+        :param float arg_perihelion: Argument of perihelion wrt ecliptic in degrees (default is None)
+        :param float inclination: Inclination wrt ecliptic in degrees (default is None)
+        :param float mean_anomaly: Mean anomaly in degrees (default is None)
+        :param float semimajor_axis: Semi-major axis in au (default is None)
+        :param float mean_motion: Mean motion in deg/d (default is None)
+        :param float mjd_start: Start time in Modified Julian Date (default is 58849.0)
+        :param float mjd_end: End time in Modified Julian Date (default is 61042.0)
+        :param str step_size: Step size for ephemeris query (default is "1d")
+        :param str quantities: Comma-separated list of quantities to request from the Horizons API (default is '1,2,3,4,5,6,7,8,9,10,19,20,23,24,25,27,29')
+        :param bool verbose: If True, prints the request URL and parameters for debugging (default is False)
+    """
+
+    from astropy.time import Time
     # Example of a HTTP API Request to Horizons: 
     request_url = "https://ssd.jpl.nasa.gov/api/horizons.api?format=text&"
     # request_url = "https://ssd.jpl.nasa.gov/api/horizons.api?"
@@ -23,8 +45,14 @@ def ephessos(designation, epoch=None, eccentricity=None, node=None, arg_periheli
     A =   str(df.iloc[0]["Semimajor_Axis"])	# au 	Semi-major axis (see note above)
     N =	  str(df.iloc[0]["Mean_Motion"]) # deg/d 	Mean motion (see note above)
     """
+    print("TODO: ping this to https://www.minorplanetcenter.net/iau/MPCORB/MPCORB.DAT")
+    print("Check tess-ephem! at github")
     OBJECT = designation # 	Name of user input object
-    EPOCH = epoch 	  	# Julian Day number (JDTDB) of osculating elements
+    if epoch == "K25BL":
+        EPOCH = Time('2025-11-21T00:00:00', format='isot', scale='tt').jd
+    else:
+        EPOCH = Time(epoch, format='isot', scale='tt').jd
+
     ECLIP = "J2000" 	  	# Reference ecliptic frame of elements: J2000 
     EC =  str(eccentricity) # 	  	Eccentricity
     # QR = 	# au 	Perihelion distance (see note above
@@ -55,7 +83,7 @@ def ephessos(designation, epoch=None, eccentricity=None, node=None, arg_periheli
     else:
         request_url = request_url + "COMMAND=';'" 
     if designation is not None: request_url = request_url + "&OBJECT="+OBJECT
-    if epoch is not None: request_url = request_url + "&EPOCH=2461000.83333"#+EPOCH
+    if epoch is not None: request_url = request_url + "&EPOCH="+str(EPOCH)
     request_url = request_url + "&ECLIP="+ECLIP
     if eccentricity is not None: request_url = request_url + "&EC="+EC 
     if node is not None: request_url = request_url + "&OM="+OM 
@@ -68,14 +96,21 @@ def ephessos(designation, epoch=None, eccentricity=None, node=None, arg_periheli
     # request_url = request_url + "&COMMAND='499'"
     request_url = request_url + "&OBJ_DATA='YES'" 
     request_url = request_url + "&MAKE_EPHEM='YES'" 
-    request_url = request_url + "&EPHEM_TYPE='OBSERVER'" 
-    request_url = request_url + "&CENTER='500@399'" 
+    request_url = request_url + "&EPHEM_TYPE='OBSERVER'"
+    print("TODO: Implement user-input observer location") 
+    print("TODO: 500@399 is geocentric, make it L2 or user input") 
+
+    request_url = request_url + "&CENTER='500@399'"  
     request_url = request_url + "&START_TIME='" + t_start.isot + "'" 
     request_url = request_url + "&STOP_TIME='"+ t_end.isot + "'" 
     request_url = request_url + "&STEP_SIZE='" + step_size + "'"
-    request_url = request_url + "&CSV_FORMAT='YES'" 
-    request_url = request_url + "&QUANTITIES='1,2,3,4,5,6,7,8,9,10,20,23,24,25,27,29'"
+    request_url = request_url + "&CSV_FORMAT='YES'"
+    print("TODO: Make it user input optional Quantities!")  
+    print("TODO: Link to the Horizons API docs for the list") 
+    request_url = request_url + "&QUANTITIES='"+ quantities + "'"  
     # request_url = request_url.replace(",", "%3B")
+    print("TODO: Add H magnitude and G slope parameter to the input")  
+
 
     import urllib.request
     if verbose: print(request_url)
@@ -130,6 +165,12 @@ def ephessos(designation, epoch=None, eccentricity=None, node=None, arg_periheli
 
 
 def translate_horizons_date_to_date_hms(horizons_date):
+    """
+    translate_horizons_date_to_date_hms translates the date format provided by the Horizons API (e.g., "2025-Jan-22 00:00") into a standard ISO format (e.g., "2025-01-22T00:00:00"). This function replaces the month abbreviations with their corresponding numeric values and formats the date string accordingly.
+        :param str horizons_date: Date string from the Horizons API (e.g., "2025-Jan-22 00:00")
+        :returns: Date string in ISO format (e.g., "2025-01-22T00:00:00")
+        :rtype: str
+    """
     # Example input: "2025-Jan-22 00:00"
     # Convert to ISO format: "2025-01-22T00:00:00"
     date_hms = horizons_date.replace("Jan","01").replace("Feb","02").replace("Mar","03").replace("Apr","04").replace("May","05").replace("Jun","06").replace("Jul","07").replace("Aug","08").replace("Sep","09").replace("Oct","10").replace("Nov","11").replace("Dec","12")
@@ -140,6 +181,12 @@ def translate_horizons_date_to_date_hms(horizons_date):
 
 
 def read_mpc_nea_file(file_path):
+    """
+    read_mpc_nea_file reads the MPCORB.DAT file from the Minor Planet Center and parses it into a pandas DataFrame. The function defines the exact character ranges for each column based on the MPC schema and uses pandas' read_fwf function to read the fixed-width formatted file. It also converts certain columns to numeric types and ensures that the Designation column is treated as a string. This function is specifically designed for reading the Near-Earth Asteroids (NEA) subset of the MPCORB.DAT file, which may have a different structure or content compared to the full MPCORB.DAT file.
+        :param str file_path: Path to the MPCORB.DAT file
+        :returns: DataFrame containing the parsed MPCORB.DAT data for Near-Earth Asteroids
+        :rtype: pandas.DataFrame
+    """
     # Define the exact character ranges based on the MPC schema
     # Note: pandas uses 0-based indexing and the 'stop' value is exclusive
     col_specification = [
@@ -194,5 +241,200 @@ def read_mpc_nea_file(file_path):
     df["Designation"] = df["Designation"].astype(str)
 
     # Preview the first few rows
-    print(df.head())
+    # print(df.head())
     return(df)
+
+
+def read_mpcorb_file(file_path, skiprows=43):
+    """
+    read_mpcorb_file reads the MPCORB.DAT file from the Minor Planet Center and parses it into a pandas DataFrame. The function defines the exact character ranges for each column based on the MPC schema and uses pandas' read_fwf function to read the fixed-width formatted file. It also converts certain columns to numeric types and ensures that the Designation column is treated as a string. The skiprows parameter allows skipping the header lines of the MPCORB.DAT file, which typically contain metadata and comments.
+        :param str file_path: Path to the MPCORB.DAT file
+        :param int skiprows: Number of header lines to skip (default is 43)
+        :returns: DataFrame containing the parsed MPCORB.DAT data
+        :rtype: pandas.DataFrame
+    """
+
+    # Define the exact character ranges based on the MPC schema
+    # Note: pandas uses 0-based indexing and the 'stop' value is exclusive
+    col_specification = [
+        (0, 7),     # Designation
+        (8, 13),    # H (Absolute Mag)
+        (14, 19),   # G (Slope Parameter)
+        (20, 25),   # Epoch
+        (26, 35),   # Mean Anomaly
+        (37, 46),   # Argument of Perihelion
+        (48, 57),   # Longitude of Ascending Node
+        (59, 68),   # Inclination
+        (70, 79),   # Eccentricity
+        (80, 91),   # Mean Daily Motion
+        (92, 103),  # Semimajor Axis
+        (105, 106), # Uncertainty (U)
+        (107, 116), # Reference
+        (117, 122), # Num observations
+        (123, 126), # Num oppositions
+        (127, 131), # First year / Arc
+        (132, 136), # Last year / 'days'
+        (137, 141), # r.m.s residual
+        (142, 145), # Perturbers (Coarse)
+        (146, 149), # Perturbers (Precise)
+        (150, 160), # Computer Name
+        (161, 165), # Flags (Hex)
+        (166, 174), # Numerical ID
+        (175, 194), # Readable Designation
+        (194, 202)  # Last Observation Date
+    ]
+
+    column_names = [
+        "Designation", "H", "G", "Epoch", "Mean_Anomaly", "Arg_Perihelion", 
+        "Node", "Inclination", "Eccentricity", "Mean_Motion", "Semimajor_Axis", 
+        "Uncertainty", "Ref", "Obs_Count", "Opp_Count", "First_Obs", "Last_Obs_Arc",
+        "RMS_Resid", "Pert_Coarse", "Pert_Precise", "Comp_Name", "Flags", "Num_ID",
+        "Full_Name", "Last_Obs_Date"
+    ]
+
+    # Read the file, skipping the header (first 43 lines)
+    df = pd.read_fwf(
+        file_path, 
+        colspecs=col_specification, 
+        names=column_names, 
+        header=None,
+        skiprows=skiprows
+    )
+
+    # Optional: Convert numeric columns that might have been read as strings
+    df['H'] = pd.to_numeric(df['H'], errors='coerce')
+    df['Semimajor_Axis'] = pd.to_numeric(df['Semimajor_Axis'], errors='coerce')
+    df["Designation"] = df["Designation"].astype(str)
+
+    return df
+
+
+
+def get_last_modification_time(file_path):
+    """
+    get_last_modification_time checks the last modification time of a file and calculates the time difference between now and the last modification time. It returns a dictionary containing the time difference (Delta), the last modification time, and the current time. This function is useful for determining if a cached file is still valid or if it needs to be updated.
+        :param str file_path: Path to the file to check
+        :returns: Dictionary containing the time difference, last modification time, and current time
+        :rtype: dict
+    """
+    import os
+    import time
+    import datetime
+    from astropy.time import Time
+
+    if os.path.exists(file_path):
+        modification_time = Time(datetime.datetime.fromtimestamp(os.path.getmtime(file_path)))
+        # print(modification_time)
+        now_time = Time.now()
+        # print(now_time)
+        delta_t  = now_time - modification_time
+
+    else:
+        print("File does not exist.")
+    
+    return({"Delta": delta_t, "Modification Time": modification_time, "Now Time": now_time})
+
+
+def download_file(url, verbose=True):
+    """
+    Downloads the file described as an url address.
+        :param str url: Url of the file to be downloaded
+        :returns: Local path of the downloaded file
+        :rtype: str
+    """
+    import requests
+    # From: https://stackoverflow.com/questions/16694907/download-large-file-in-python-with-requests
+    local_filename = url.split('/')[-1]
+    # NOTE the stream=True parameter below
+    from tqdm import tqdm 
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        total_size = int(r.headers.get('content-length', 0))
+        with open(local_filename, 'wb') as f:
+            with tqdm(total=total_size, unit='B', unit_scale=True, disable=not verbose) as pbar:
+                for chunk in r.iter_content(chunk_size=1048576):
+                    if chunk:
+                        f.write(chunk)
+                        pbar.update(len(chunk))
+    return(local_filename)
+
+# If needed. Download the MPCORB.DAT file and read it into a table
+
+
+def get_last_mpcorb():
+    """
+    get_last_mpcorb checks if the MPCORB.DAT file exists in the cache and if it is less than a day old. If it exists and is recent, it uses the cached file. If it does not exist or is old, it downloads the latest MPCORB.DAT.gz file from the Minor Planet Center, saves it to the cache, and then reads it into a DataFrame. The function returns the path to the uncompressed MPCORB.DAT file that can be read into a DataFrame.
+
+    :returns: Path to the uncompressed MPCORB.DAT file
+    :rtype: str
+    """
+    # Check if file exists in cache, if not download it and move it to the cache
+    import gzip
+    import shutil
+    
+    # If the file exists ... 
+    mpcorb_cache_path = os.path.join(ep.core.ephessos_dir, "MPCORB.DAT.gz")
+    if os.path.exists(mpcorb_cache_path):
+        last_modification_time = get_last_modification_time(mpcorb_cache_path)
+        if last_modification_time["Delta"].sec < 3600*24:  # If the file is less than a one day old, use it
+            print(f"Using Minor Planet Center cached file: Last: {last_modification_time['Modification Time'].isot}")
+            with gzip.open(mpcorb_cache_path, 'rb') as f_in:
+                with open(mpcorb_cache_path.replace(".gz",""), 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)    
+                    return(f_out.name)
+        
+    # If not, or if the file is old 
+    else:
+        mpcorb_url = "https://www.minorplanetcenter.net/iau/MPCORB/MPCORB.DAT.gz"
+        download_file(mpcorb_url, verbose=True)
+        os.rename("MPCORB.DAT.gz", mpcorb_cache_path)
+        # print(os.path.join(ep.core.ephessos_dir, "MPCORB.DAT.gz"))
+        with gzip.open(mpcorb_cache_path, 'rb') as f_in:
+            with open(mpcorb_cache_path.replace(".gz",""), 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)    
+                return(f_out.name)
+            
+
+def cone_search(ra, dec, epoch, search_radius):
+    """
+    cone_search performs a cone search around the given RA, Dec, and epoch with the specified search radius to find potential matches in the MPCORB.DAT file. It uses the pympc library to perform the initial cone search and then cross-references the results with the MPCORB.DAT file to find corresponding entries.
+        :param float ra: Right Ascension in degrees
+        :param float dec: Declination in degrees
+        :param float epoch: Epoch in Julian Date
+        :param float search_radius: Search radius in arcseconds
+        :returns: DataFrame containing the matching entries from MPCORB.DAT
+        :rtype: pandas.DataFrame
+    """
+
+    # Get the path to the latest MPCORB.DAT file
+    last_mpcorb = ep.core.get_last_mpcorb()
+
+    # Perform the cone search using pympc to get potential matches
+    import pympc
+    matches = pympc.minor_planet_check(ra=ra, dec=dec, epoch=epoch, search_radius=search_radius)
+
+    # Find the corresponding entries in MPCORB.DAT for the matches found by pympc
+    return(find_sso_in_mpcorb(matches, last_mpcorb))
+
+
+def find_sso_in_mpcorb(matches, last_mpcorb):
+    """
+    find_sso_in_mpcorb finds the corresponding entries in MPCORB.DAT for the matches found by pympc.
+    :param pandas.DataFrame matches: DataFrame containing the matches found by pympc
+    :param str last_mpcorb: Path to the latest MPCORB.DAT file
+    :returns: DataFrame containing the matching entries from MPCORB.DAT
+    :rtype: pandas.DataFrame
+    """
+
+    # Find the line inside MPCORB.DAT that corresponds to the object of interest
+    temp_last_mpcorb = last_mpcorb.replace(".DAT", "_temp.DAT")
+
+    sso_matches = []
+    from tqdm import tqdm 
+    for i in tqdm(range(len(matches["name"]))):
+        os.system('grep "' + str(matches["name"][i]) + '" ' + last_mpcorb + ' > ' + temp_last_mpcorb)
+        # Read the temporary file containing the single SSO into a DataFrame
+        sso_matches.append(ep.core.read_mpcorb_file(file_path=temp_last_mpcorb, skiprows=0))
+
+    import pandas as pd
+    return(pd.concat(sso_matches))
